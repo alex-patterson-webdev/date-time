f15069b000f660050199ef3a854ec1922e7b5b8f
Alex Patterson <alex.patterson.webdev@gmail.com>
2019-08-11 19:55:09 +0000

    Updated existing DateTime view helper.

--- src/View/Helper/DateTime.php
+++ src/View/Helper/DateTime.php
@@ -7,6 +7,8 @@ use Zend\View\Helper\AbstractHelper;
 /**
  * DateTime
  *
+ * View helper used to render a \DateTime instance to a specific string format.
+ *
  * @author  Alex Patterson <alex.patterson.webdev@gmail.com>
  * @package Arp\DateTime\View\Helper
  */
@@ -20,18 +22,30 @@ class DateTime extends AbstractHelper
     protected $format = 'd/m/y H:m:s';
 
     /**
+     * __construct.
+     *
+     * @param array $options
+     */
+    public function __construct(array $options = [])
+    {
+        if (! empty($options['format'])) {
+            $this->setFormat($options['format']);
+        }
+    }
+
+    /**
      * __invoke
      *
      * Render the date and time as a string.
      *
-     * @param \DateTime|null $dateTime
-     * @param array          $options
+     * @param \DateTime|null $dateTime  The date and time instance to format.
+     * @param array          $options   The optional rendering options.
      *
      * @return string
      */
-    public function __invoke(\DateTime $dateTime = null, array $options = [])
+    public function __invoke(\DateTime $dateTime = null, array $options = []) : string
     {
-        if (null == $dateTime) {
+        if (null === $dateTime) {
             return '';
         }
 
@@ -41,14 +55,30 @@ class DateTime extends AbstractHelper
     /**
      * render
      *
-     * @param \DateTime $dateTime
-     * @param array     $options
+     * Render the \DateTime instance as a string.
+     *
+     * @param \DateTime $dateTime  The date and time instance to format.
+     * @param array     $options   The optional rendering options.
      *
      * @return string
      */
-    public function render(\DateTime $dateTime, array $options = [])
+    public function render(\DateTime $dateTime, array $options = []) : string
     {
-        return $dateTime->format($this->format);
+        $format = isset($options['format']) ? $options['format'] : $this->format;
+
+        return $dateTime->format($format);
+    }
+
+    /**
+     * setFormat
+     *
+     * Set the view helper date time format.
+     *
+     * @param string $format
+     */
+    public function setFormat(string $format)
+    {
+        $this->format = $format;
     }
 
 }
