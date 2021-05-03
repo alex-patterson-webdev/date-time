861d957af99af8af3ca46fc2556233eaf88bdba3
Alex Patterson <alex.patterson.webdev@gmail.com>
2021-05-03 14:23:04 +0000

    Added new DateTimeImmutableFactory

---
+++ test/unit/DateTimeImmutableFactoryTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ArpTest\DateTime;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Arp\DateTime\DateTimeImmutableFactory
+ *
+ * @author  Alex Patterson <alex.patterson.webdev@gmail.com>
+ * @package ArpTest\DateTime
+ */
+final class DateTimeImmutableFactoryTest extends TestCase
+{
+
+}
